0cfa1a6c25c18729813593fabe9b382ee03b3b23
Aljur-Pavel-Dubina <pavel.dubina@aljur.com>
2020-11-23 16:51:12 +0300

    Create Error handler, Change environment to Production

--- src/WebAPIWorkTracker/Startup.cs
+++ src/WebAPIWorkTracker/Startup.cs
@@ -56,6 +56,11 @@ namespace WebAPIWorkTracker
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIWorkTracker v1"));
+
+            }
+            else
+            {
+                app.UseExceptionHandler("/error");
             }
 
             app.UseHttpsRedirection();
