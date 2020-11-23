6cfcc3681cea52b2c994b657768d19db00a19313
JUK-ALJUR <krasnopolskaya.julia@aljur.com>
2020-11-23 13:34:16 +0200

    Configs

--- src/Domain/DataModels/RX_Job.cs
+++ src/Domain/DataModels/RX_Job.cs
@@ -7,8 +7,7 @@ namespace Domain.DataModels
 	public class RX_Job
 	{
 		public Guid Id { get; set; }
-		public int? ContractorID { get; set; }
-		
+		public int? ContractorID { get; set; }		
 		public string Name { get; set; } 
 		public  string Status { get; set; } 
 		public int? Floor { get; set; }
